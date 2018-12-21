86058e62477852aa0c21bfe3efeec20ba2a32913
universtounding <kendmorse@gmail.com>
2018-12-20 23:54:02 -0500

    added button links for home and about me

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'welcome/home', to: 'welcome#home'
-  get 'welcome/about_me', to: 'welcome#about_me'
+  get 'welcome/about', to: 'welcome#about'
 end
